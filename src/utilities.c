227e94b73aec8dbdcadac3f0060b9c82dbe90e7f
4carducci <acarducci@ogs.it>
2023-11-23 16:45:05 +0100

    created files for splitted functions

---
+++ src/utilities.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+
+
+static void azzero(double *d, const int n)
+{
+    int i;
+    for (i=0; i<n; ++i) {
+        d[i]=0.0;
+    }
+}
+
+/* helper function: apply minimum image convention */
+static double pbc(double x, const double boxby2)
+{
+    while (x >  boxby2) x -= 2.0*boxby2;
+    while (x < -boxby2) x += 2.0*boxby2;
+    return x;
+}
