d66b9bc4304a04a6cd54f19e584d256020f1a924
4carducci <acarducci@ogs.it>
2023-11-23 19:04:36 +0100

    init.h

---
+++ inc/init.h
@@ -0,0 +1,14 @@
+#include "structs.h"
+
+#ifndef INIT_H
+#define INIT_H
+  #ifdef __cplusplus
+  extern "C" {
+   #endif
+      
+    void init(mdsys_t sys);
+  
+  #ifdef __cplusplus
+    }
+   #endif
+#endif
