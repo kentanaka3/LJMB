a13842b434f979d9b66d8f740493cc54e313242b
4carducci <acarducci@ogs.it>
2023-11-23 18:34:44 +0100

    updated init, removed allocate

--- src/init.c
+++ src/init.c
@@ -1,4 +1,11 @@
-#include "structs_prots.hpp"
+#include "structs.h"
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <math.h>
+#include <sys/time.h>
 
 void init(){
     /* read input file */
