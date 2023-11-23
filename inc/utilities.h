68d9e45ff50076b5bc8cff152968b31e7cecb541
4carducci <acarducci@ogs.it>
2023-11-23 23:53:49 +0100

    fixed utilities.h

--- inc/utilities.h
+++ inc/utilities.h
@@ -1,12 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 #ifndef UTILITIES_H
 #define UTILITIES_H
   #ifdef __cplusplus
   extern "C" {
    #endif
       
-    int get_a_line(FILE *fp, char *buf);
-    void azzero(double *d, const int n);
-    double pbc(double x, const double boxby2);
+    int get_a_line(FILE *, char *);
+    void azzero(double *, const int);
+    double pbc(double, const double);
   
   #ifdef __cplusplus
     }
