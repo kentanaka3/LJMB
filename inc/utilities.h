ce09ce40b32f47cabb7fb998df9981577f734a23
Ken Tanaka Hernández <ktanaka@ictp.it>
2023-11-23 19:07:34 +0100

    Add LJMD Version

--- inc/utilities.h
+++ inc/utilities.h
@@ -1,15 +1,15 @@
 #ifndef UTILITIES_H
-#define UTILITIES_H
+  #define UTILITIES_H
+
   #ifdef __cplusplus
-  extern "C" {
-   #endif
-      
+    extern "C" {
+  #endif
     int get_a_line(FILE *fp, char *buf);
     void azzero(double *d, const int n);
     double pbc(double x, const double boxby2);
-  
+
   #ifdef __cplusplus
     }
-   #endif
+  #endif
 #endif
 
