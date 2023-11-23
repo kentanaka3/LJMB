14dedd2d6ce12b0b47551d1e281cb1c46a04094b
4carducci <acarducci@ogs.it>
2023-11-23 18:11:56 +0100

    fixed header for utilities

--- inc/utilities.h
+++ inc/utilities.h
@@ -1,13 +1,13 @@
-#ifndef protos
+#ifndef utilities_
 #ifdef __cplusplus
 extern "C" {
     #endif
-    #define protos  
+    #define utilities_  
     int get_a_line(FILE *fp, char *buf);
     void azzero(double *d, const int n);
     double pbc(double x, const double boxby2);
     #ifdef __cplusplus
-}
-#endif
+    }
+    #endif
 #endif
 
