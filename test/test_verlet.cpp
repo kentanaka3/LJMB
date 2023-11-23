699a36209149c595ec1c95ca0c143a3b54b97eda
Ken Tanaka Hernández <ktanaka@ictp.it>
2023-11-23 17:19:02 +0100

    Added Tests for utils and verlet

--- test/test_verlet.cpp
+++ test/test_verlet.cpp
@@ -1,1 +1,46 @@
 #include "gtest/gtest.h"
+#include "verlet.c"
+
+class VerletTest:
+  public::testing::Test {
+    protected:
+    mdsys_t * sys;
+    void Setup() {
+      sys = new mdsys_t;
+      sys->natoms = 2;
+      sys->mass = 1.0;
+      sys->rx = new double[2];
+      sys->vx = new double[2];
+      sys->fx = new double[2];
+      sys->rx[0] = -1.0;
+      sys->rx[1] = 1.0;
+      sys->vx[0] = 0.0;
+      sys->vx[1] = 0.0;
+      sys->fx[0] = 1.0;
+      sys->fx[1] = 0.2;
+    }
+    void TearDown() {
+      delete[] sys->rx;
+      delete[] sys->vx;
+      delete[] sys->fx;
+
+      delete sys;
+    }
+  };
+  TEST_F(VerletTest, step1) {
+    ASSERT_NE(sys, nullptr);
+    ASSERT_DOUBLE_EQ(sys->rx[0], -1.0);
+    ASSERT_DOUBLE_EQ(sys->vx[0], 0.0);
+    verlet_1(sys);
+    ASSERT_DOUBLE_EQ(sys->rx[0],-0.5);
+    ASSERT_DOUBLE_EQ(sys->vx[0], 0.5);
+  }
+
+  TEST_F(VerletTest, step2) {
+    ASSERT_NE(sys,nullptr);
+    ASSERT_DOUBLE_EQ(sys->rx[0], -1.0);
+    ASSERT_DOUBLE_EQ(sys->vx[0], 0.0);
+    verlet_2(sys);
+    ASSERT_DOUBLE_EQ(sys->rx[0], -1.0);
+    ASSERT_DOUBLE_EQ(sys->vx[0], 0.5);
+  }
