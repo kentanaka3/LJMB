06e4637c825d613e6ba4011134e0a6cec77735b3
4carducci <acarducci@ogs.it>
2023-11-24 10:29:12 +0100

    updated verlet.h

--- inc/verlet.h
+++ inc/verlet.h
@@ -5,8 +5,8 @@
   #ifdef __cplusplus
   extern "C" {
    #endif
-      void velverlet_part1(mdsys_t *);
-      void velverlet_part2(mdsys_t *);
+      void velverlet(mdsys_t *);
+      void velverlet_prop(mdsys_t *);
   
   #ifdef __cplusplus
     }
