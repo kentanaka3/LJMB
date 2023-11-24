0643c214d9453a596940a5821e84b540e54ec39b
4carducci <acarducci@ogs.it>
2023-11-24 09:27:19 +0100

    updated verlet.h

--- inc/verlet.h
+++ inc/verlet.h
@@ -1,11 +1,12 @@
+#include "structs.h"
 
 #ifndef VERLET_H
 #define VERLET_H
   #ifdef __cplusplus
   extern "C" {
    #endif
-      
-    
+      void verlet_part1(mdsys_t *);
+      void verlet_part2(mdsys_t *);
   
   #ifdef __cplusplus
     }
