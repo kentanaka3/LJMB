acec8031a12cacc7bb109355b315a3abf655f17d
4carducci <acarducci@ogs.it>
2023-11-25 17:15:41 +0100

    temptative PBC_Test in test_utils.cpp

--- test/test_utils.cpp
+++ test/test_utils.cpp
@@ -12,3 +12,39 @@ TEST(TestAzzero, doubles) {
   ASSERT_DOUBLE_EQ(buf[5], 0.0);
   ASSERT_DOUBLE_EQ(buf[9], 0.0);
 }
+
+class VerletTest:
+  public::testing::Test {
+    protected:
+    mdsys_t * sys;
+    void SetUp() {
+      sys = new mdsys_t;
+      sys->natoms = 2;
+      sys->rx = new double[2];
+      sys->ry = new double[2];
+      sys->rz = new double[2];
+      sys->rx[0] = -1.0;
+      sys->rx[1] = 1.0;
+      sys->ry[0] = -1.0;
+      sys->ry[1] = 1.0;
+      sys->rz[0] = -1.0;
+      sys->rz[1] = 1.0;
+      sys->10;
+    }
+    void TearDown() {
+      delete[] sys->rx;
+      delete[] sys->ry;
+      delete[] sys->rz;
+
+      delete sys;
+    }
+  };
+
+TEST(TestPBC, doubles) {
+    int i=0;
+    int j=1;
+    rx=pbc(sys->rx[i] - sys->rx[j], 0.5*sys->box);
+    ry=pbc(sys->ry[i] - sys->ry[j], 0.5*sys->box);
+    rz=pbc(sys->rz[i] - sys->rz[j], 0.5*sys->box);
+    ASSERT_DOUBLE_EQ(rx*rx+ry*ry+rz*rz,192);
+}
