ff1f00053268b56d391fda3a9963b1f7ab11d2be
ktanakah <kentanaka3@outlook.com>
2023-11-30 22:22:48 +0100

    Minor program changes

--- src/comp.c
+++ src/comp.c
@@ -6,9 +6,8 @@
 #if defined (_OPENMP)
 #include <omp.h>
 #endif
-
 #ifdef MORSE
-
+#include <math.c>
 #endif
 
 /* Compute Kinetic Energy */
@@ -66,15 +65,18 @@ void force(mdsys_t *sys) {
       double rsq = rx*rx + ry*ry + rz*rz;
       /* Compute Force and Energy if within cutoff */
       if (rsq < rcsq) {
-        /* Morse Potential
-        MM = exp(- sys->a * (r - sys->re));
-        // Mores potential
-        sys->epot += sys->m + sys->De * (1-MM)*(1-MM);
-        ffac = 2 * sys->a * sys->De * MM * (1-MM);
-         */
+        #ifdef MORSE
+        // Morse Potential
+        MM = exp(-sys->a * (r - sys->re));
+        // Morse potential
+        epot += sys->m + sys->De * (1 - MM)*(1 - MM);
+        double ffac = 2 * sys->a * sys->De * MM * (1 - MM);
+        #else
+        // LJ Potential
         double rinv = ((1.0 / rsq) / rsq) / rsq;
         double ffac = 6.0*(2.0*c12*rinv - c6)*rinv/rsq;
         epot += (c12*rinv - c6)*rinv;
+        #endif
 
         fx[ii] += rx*ffac;
         fy[ii] += ry*ffac;
